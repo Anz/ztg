2b47a3ed1cb834aa17353336c52253c69aa0d382
anz <knecht_roger@hotmail.com>
2012-04-23 13:39:47 +0200

    improved switches

--- ztg/src/Switch.js
+++ ztg/src/Switch.js
@@ -1,24 +1,37 @@
 var Switch = Class.create(Entity, {
 	onCreate: function (attributes) {
 		this.layer = this.getValue(attributes.layer, 0.1);
+		this.speed = this.getValue(attributes.speed, 100);
 	},
 	onStep: function (camera) {
 		if (typeof(this.opened) != 'undefined') {
-			var now = new Date().getTime();
-			if (now - this.opened > 100) {
+			var step = Math.round((new Date().getTime() - this.opened) / this.speed);
+			if (step < this.frames) {
+				this.framex = step;
+			} else {
+				this.framex = this.frames - 1;
+			}
+			
+			/*if (now - this.opened > 100) {
 				this.framex = 1;
 			}
 			if (now - this.opened > 200) {
 				this.framex = 2;
-			}
+			}*/
 		} else if (typeof(this.closed) != 'undefined') {
-			var now = new Date().getTime();
-			if (now - this.closed > 100) {
+			var step = Math.round((new Date().getTime() - this.closed) / this.speed);
+			if (step < this.frames) {
+				this.framex = this.frames - step - 1;
+			} else {
+				this.framex = 0;
+			}
+			//this.framex = this.frames-(((now - this.opened)/100)%this.frames);
+			/*if (now - this.closed > 100) {
 				this.framex = 1;
 			}
 			if (now - this.closed > 200) {
 				this.framex = 0;
-			}
+			}*/
 		}
 	},
 	onAction: function () {
